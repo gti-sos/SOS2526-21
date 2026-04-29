1c51aea880a5a3b2bd2991056d5d9171a30e8cba
ivan7112 <ivaarrdom@alum.us.es>
2026-04-30 01:49:16 +0200

    macro push pokemone

--- src/frontend/src/routes/integrations/+page.svelte
+++ src/frontend/src/routes/integrations/+page.svelte
@@ -2,6 +2,8 @@
 import Highcharts from "highcharts";
 import { dev } from '$app/environment';
 import { onMount } from 'svelte';
+	import { randomBytes } from "node:crypto";
+	import { listen } from "node:quic";
 
 
 
@@ -83,6 +85,132 @@ async function getDatosGatos(){
 
     
 
+}
+
+//--Pokemone
+let BASE_API_POKEMONE="https://pokeapi.co/api/v2/";
+
+let stats=["hp","attack","defense","special-attack","special-defense","speed"];
+let statElegida=$state("");
+
+let resultado=$state("");
+let racha=$state(0);
+
+
+let pokemon1=$state("");
+let fotopk1=$state("");
+let statpk1=$state(0);
+let gritopk1=$state("");
+
+
+let pokemon2=$state("");
+let fotopk2=$state("");
+let statpk2=$state(0);
+let gritopk2=$state("");
+
+let idMin=1;
+let idMax=1000;
+
+//-----Funcion Pokemone
+
+async function getDatosPokemone(){
+
+   let id1=randomInt(idMin,idMax); //Id de los pokemon para solicitar a la API
+   let id2=randomInt(idMin,idMax);
+
+   let res1=await fetch(BASE_API_POKEMONE+`/pokemon/${id1}`,{method:'GET'});
+   let pk1=await res1.json();
+
+   let res2=await fetch(BASE_API_POKEMONE+`/pokemon/${id2}`,{method:'GET'});
+   let pk2=await res2.json();
+
+   let stat=randomItem(stats);
+   statElegida=stat;
+   
+
+   //Sacamos la stat de los Pokemon deseada y su foto, asi como el grito
+
+   let foto1=pk1.sprites.front_default;
+   let nombre1=pk1.name;
+   let stats1=pk1.stats;
+   let grito1=pk1.cries.latest;
+
+   let stat1=0;
+
+   for (let i of stats1){
+    if(stat===i.stat.name) stat1=i.base_stat;
+   }
+
+
+
+
+   let foto2=pk2.sprites.front_default;
+   let nombre2=pk2.name;
+   let stats2=pk2.stats;
+   let grito2=pk2.cries.latest;
+
+   let stat2=0;
+   for (let i of stats2){
+    if(stat===i.stat.name) stat2=i.base_stat;
+   }
+
+   
+
+   //Asignamos los atributos a las variables globales
+
+   pokemon1=nombre1;
+   fotopk1=foto1;
+   statpk1=stat1;
+   gritopk1=grito1;
+
+
+   pokemon2=nombre2;
+   fotopk2=foto2;
+   statpk2=stat2;
+   gritopk2=grito2;
+
+   console.log("stat1:",statpk1);
+   console.log("stat2:",statpk2);
+
+}
+
+async function decidePregunta(primero){
+    if(primero){
+        if(statpk1>=statpk2){ 
+
+            resultado="CORRECTO";
+            racha=racha+1;
+            let audio=new Audio(gritopk1);
+            audio.play();
+
+        }
+        else{resultado="Que Pena, Has Fallado"; racha=0;}
+        
+    }else{
+        if(statpk1>=statpk2){ resultado="Que Pena, Has Fallado"; racha=0;}
+        else{
+            resultado="CORRECTO";
+            racha=racha+1;
+            let audio=new Audio(gritopk2);
+            audio.play();
+        }
+        
+    }
+    getDatosPokemone();
+}
+
+
+
+
+
+//-----Funcs auxiliares
+
+function randomInt(idmin, idmax) {
+  return Math.floor(Math.random() * (idmax - idmin + 1)) + idmin;
+}
+
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 //María
@@ -230,6 +358,36 @@ happines_chart=Highcharts.chart('g15-happiness-indices', {
 </article>
 
 </div>
+
+
+<h3>Preguntas de Pokemones</h3>
+
+<button onclick={getDatosPokemone}>Quiero Jugar ^^</button>
+
+<div>¿Qué Pokémon tiene más {statElegida}?</div>
+
+<div>
+    Tu racha es de {racha} puntos;
+</div>
+<div>
+    {resultado}
+</div>
+<div class="card">
+    <div>
+        {pokemon1}
+        <img src={fotopk1} alt="pkmn1 aquí">
+
+        <button onclick={()=>decidePregunta(true)}>Elijo el Primero</button>
+    </div>
+    <div>
+        {pokemon2}
+        <img src={fotopk2} alt="pkmn2 aquí">
+        <button onclick={()=>decidePregunta(false)}>Elijo el segundo </button>
+    </div>
+    
+</div>
+
+
 
 
 <h2>Integraciones de María Torres Chacón</h2>
