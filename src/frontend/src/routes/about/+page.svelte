ca8daea613c286fede9b8e6fd0a5ec77157e8f15
QXB8295 <martorcha@alum.us.es>
2026-05-11 13:25:50 +0200

    videito

--- src/frontend/src/routes/about/+page.svelte
+++ src/frontend/src/routes/about/+page.svelte
@@ -2,6 +2,6 @@
 
 <h2>Religious-believes-stats</h2>
 
-<h2>Cholera-stats</h2>
+<h2>María Torres Chacón:</h2> <a href="https://drive.google.com/file/d/1uVHmU-S9Bf_2-cP7EfrPClFAP4B222rI/view?usp=sharing">VIDEO Cholera-stats</a>
 
 <h2>AIDS-deaths-stats</h2>
