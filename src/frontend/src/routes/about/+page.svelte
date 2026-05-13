2360894fd0d77fd2e4c831630775f0e326e00d38
QXB8295 <martorcha@alum.us.es>
2026-05-13 19:08:57 +0200

    video religioso

--- src/frontend/src/routes/about/+page.svelte
+++ src/frontend/src/routes/about/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Enlaces a los Vídeos del entregable D03</h1>
 
-<h2>Religious-believes-stats</h2>
+<h2>Religious-believes-stats</h2> <a href= "https://drive.google.com/file/d/1Yj0QWZD0L5V9S_T-YKYKfk9-ovja_RV8/view?usp=sharing">VIDEO religious-believes-stats</a>
 
 <h2>María Torres Chacón:</h2> <a href="https://drive.google.com/file/d/1uVHmU-S9Bf_2-cP7EfrPClFAP4B222rI/view?usp=sharing">VIDEO Cholera-stats</a>
 
-<h2>AIDS-deaths-stats</h2>
+<h2>AIDS-deaths-stats</h2> 
