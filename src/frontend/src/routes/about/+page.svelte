4a95e09e65887588de2df26a01e60f57407d8dc9
ivan7112 <ivaarrdom@alum.us.es>
2026-05-14 11:46:28 +0200

    apaño

--- src/frontend/src/routes/about/+page.svelte
+++ src/frontend/src/routes/about/+page.svelte
@@ -6,7 +6,6 @@
 <h2>Religious-believes-stats</h2> <a href= "https://drive.google.com/file/d/1Yj0QWZD0L5V9S_T-YKYKfk9-ovja_RV8/view?usp=sharing">VIDEO religious-believes-stats</a>
 
 
-<a href="https://drive.google.com/file/d/1Yj0QWZD0L5V9S_T-YKYKfk9-ovja_RV8/view?usp=sharing">Vídeo Creencias Religiosas Aquí</a>
 
 <h2>María Torres Chacón:</h2> <a href="https://drive.google.com/file/d/1uVHmU-S9Bf_2-cP7EfrPClFAP4B222rI/view?usp=sharing">VIDEO Cholera-stats</a>
 
